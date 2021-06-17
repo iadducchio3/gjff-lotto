ee555cfbfe957fe3b47ff0eb867b3dad06b9b68b
Isaac Adducchio <iadducchio@assurecare.com>
2021-06-16 21:59:43 -0400

    upgrade to angular 11

--- src/app/components/lottery-complete/lottery-complete.component.spec.ts
+++ src/app/components/lottery-complete/lottery-complete.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LotteryCompleteComponent } from './lottery-complete.component';
 
@@ -6,7 +6,7 @@ describe('LotteryCompleteComponent', () => {
   let component: LotteryCompleteComponent;
   let fixture: ComponentFixture<LotteryCompleteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LotteryCompleteComponent ]
     })
